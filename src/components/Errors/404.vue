5fff3b2d5e0afc1425cbd9bdbbe78de594a76b59
ArielEspinoza07 <arielespinoza46@gmail.com>
2018-07-13 11:33:59 -0600

    add errors components

---
+++ src/components/Errors/404.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row">
+      <div class="col-12">
+        <div class="card text-white">
+          <img  class="card-img" alt="Card Image" src="https://cdn-images-1.medium.com/max/1600/0*w_RSnw5hMAUTXf6H.jpg">
+          <div class="card-img-overlay">
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  //src="https://www.1and1.es/digitalguide/fileadmin/DigitalGuide/Teaser/not-found-t.jpg"
+  //src="https://tincan.co.uk/sites/default/files/styles/banner_image_tincan/public/images/Blog-404-01.png?itok=G8jyKL7k"
+    export default {
+        name: 'error-404',
+        data() {
+            return {}
+        },
+        created() {
+
+        },
+        mounted() {
+
+        },
+        methods: {},
+        components: {}
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Errors/500.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="card text-white">
+        <img src="https://cdn.dribbble.com/users/34790/screenshots/1638786/uh_oh.png" class="card-img" alt="Card Image">
+        <div class="card-img-overlay">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'error-500',
+        data() {
+            return {}
+        },
+        created() {
+
+        },
+        mounted() {
+
+        },
+        methods: {},
+        components: {}
+    }
+</script>
+
+<style>
+
+</style>
